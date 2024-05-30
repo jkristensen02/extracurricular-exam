4ebd9088a603d5e5ddb95e82a70fe2f898dcf71d
Julia Kristensen <julia.kristensen05@gmail.com>
2024-05-30 16:54:17 +0200

    r

--- app.js
+++ app.js
@@ -5,18 +5,51 @@ window.addEventListener("load", initApp); // When the page is loaded, run initAp
 // Function to initialize the Web App
 async function initApp() {
   console.log("initApp: app.js is running 🎉"); // Log to the console that the app is running
-  const posts = await getPosts();
-  console.log(posts);
-  displayPosts(posts);
+  const projects = await getProjects(); // Corrected the function name
+  console.log(projects);
+  // displayProjects(projects);
+  displayProjectsGrid(projects);
 }
-async function getPosts(){
-  const response = await fetch("http://extracurricularexam.cafesite.dk/wp-json/wp/v2/posts?acf_format=standard");
+
+async function getProjects() {
+  const response = await fetch(
+    "http://extracurricularexam.cafesite.dk/wp-json/wp/v2/projects?acf_format=standard"
+  );
   const data = await response.json();
   return data;
 }
-function displayPosts(posts) {
-  const postsList = document.querySelector("#posts-list");
-  for (const post of posts) {
-    console.log(post);
+
+function displayProjects(projects) {
+  const projectsList = document.querySelector("#project-list");
+
+  for (const project of projects) {
+    // Corrected the parameter name in the for...of loop
+    projectsList.insertAdjacentHTML(
+      "beforeend",
+      `
+      <li>${project.title.rendered} </li>
+      `
+    );
+  }
+}
+
+function displayProjectsGrid(projects) {
+  const projectsGrid = document.querySelector("#project-grid");
+  for (const project of projects) {
+    // Corrected the parameter name in the for...of loop
+    projectsGrid.insertAdjacentHTML(
+      "beforeend",
+      `    
+    <article class="grid-item">
+      <img src="${project.acf.image}" alt="${project.title.rendered}"></img>
+      <h2>${project.title.rendered}</h2>
+      <p>${project.acf.description}</p>
+     <p>${project.acf.client}</p>
+    
+       <p>${project.acf.type}</p>
+  <p><a href="${project.acf.link}" target="_blank">View Project</a></p>
+    </article>
+    `
+    );
   }
 }
