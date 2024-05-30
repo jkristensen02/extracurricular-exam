aabafd161d305c3c741d9da2233515b1bdb4467d
Julia Kristensen <julia.kristensen05@gmail.com>
2024-05-30 14:38:10 +0200

    t

--- app.js
+++ app.js
@@ -5,11 +5,12 @@ window.addEventListener("load", initApp); // When the page is loaded, run initAp
 // Function to initialize the Web App
 async function initApp() {
   console.log("initApp: app.js is running 🎉"); // Log to the console that the app is running
-  const posts = await getPosts();
-  console.log(posts);
+  getPosts();
+  //console.log(posts);
 }
 async function getPosts(){
-  const response = await fetch("http://http://extracurricularexam.cafesite.dk/wp-json/wp/v2/posts?acf_format=standard");
+  const response = await fetch("http://extracurricularexam.cafesite.dk/wp-json/wp/v2/posts?acf_format=standard");
   const data = await response.json();
-  return data;
+  console.log(data);
+  //return data;
 }
