c077a2d4f1fe75cb892f6a29d9dde4cf518d68c7
Julia Kristensen <julia.kristensen05@gmail.com>
2024-05-30 16:53:05 +0200

    c

--- app.js
+++ app.js
@@ -7,9 +7,16 @@ async function initApp() {
   console.log("initApp: app.js is running 🎉"); // Log to the console that the app is running
   const posts = await getPosts();
   console.log(posts);
+  displayPosts(posts);
 }
 async function getPosts(){
   const response = await fetch("http://extracurricularexam.cafesite.dk/wp-json/wp/v2/posts?acf_format=standard");
   const data = await response.json();
   return data;
+}
+function displayPosts(posts) {
+  const postsList = document.querySelector("#posts-list");
+  for (const post of posts) {
+    console.log(post);
+  }
 }
